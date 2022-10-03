fe4bb2bef0d101302bc1835ca82b2a67d5f763cc
Mateus Lippi <mateus.lippi01@gmail.com>
2022-10-03 18:22:23 -0300

    Final

--- main.c
+++ main.c
@@ -7,9 +7,8 @@
 
 int main()
 {
-
-    setlocale(LC_ALL, "Portuguese");
     //"Utilizando caracteres e acentuação da língua portuguesa
+    setlocale(LC_ALL, "Portuguese");
 
     //Apresentação do aluno:
     printf("Autor: Mateus Lippi Pereira\n");
@@ -20,7 +19,7 @@ int main()
 
     //Declaração das variáveis que serão utilizadas ao decorrer do algoritmo:
     int operacao;
-    double valor1, valor2, soma, subtracao, multiplicacao, divisao, potencia, raiz;
+    float valor1, valor2, soma, subtracao, multiplicacao, divisao, potencia, raiz;
 
     printf("----------------------------------------------------------\n");
     printf("\n");
@@ -38,77 +37,85 @@ int main()
     printf("Digite o número de acordo com a operação desejada: ");
 
     //Lendo a escolha do usuário:
+
     scanf("%d", &operacao);
 
     //Utilização da função "switch" para criar condições baseadas na entrada do usuário atribuída a variável "operacao".
-    switch (operacao)  {
+    switch (operacao)
+    {
 
-        //Primeiro caso: Uusário escolheu a operação de soma:
-        case 1:
-            printf("Digite o primeiro número: ");
-            scanf("%lf", &valor1);
-            printf("Digite o segundo número: ");
-            scanf("%lf", &valor2);
-            soma = valor1 + valor2;
-            printf("\n");
-            printf("O resultado da soma de %.2lf e %.2lf é: %.2lf", valor1, valor2, soma);
-            break;
+    //Primeiro caso: Uusário escolheu a operação de soma:
+    case 1:
+        printf("Digite o primeiro número: ");
+        scanf("%f", &valor1);
+        printf("Digite o segundo número: ");
+        scanf("%f", &valor2);
+        soma = valor1 + valor2;
+        printf("\n");
+        printf("O resultado da soma de %.2f e %.2f é: %.2f", valor1, valor2, soma);
+        break;
 
-        //Segundo caso: Usuário escolheu a operação de subtração:
-        case 2:
-            printf("Digite o primeiro número: ");
-            scanf("%lf", &valor1);
-            printf("Digite o segundo número: ");
-            scanf("%lf", &valor2);
-            subtracao = valor1 - valor2;
-            printf("\n");
-            printf("O resultado da subtração de %.2lf e %.2lf é: %2.lf", valor1, valor2, subtracao);
-            break;
+    //Segundo caso: Usuário escolheu a operação de subtração:
+    case 2:
+        printf("Digite o primeiro número: ");
+        scanf("%f", &valor1);
+        printf("Digite o segundo número: ");
+        scanf("%f", &valor2);
+        subtracao = valor1 - valor2;
+        printf("\n");
+        printf("O resultado da subtração de %.2f e %.2f é: %.2f", valor1, valor2, subtracao);
+        break;
 
-        //Terceiro caso: Usuário escolheu a operação de multiplicação:
-        case 3:
-            printf("Digite o primeiro número: ");
-            scanf("%lf", &valor1);
-            printf("Digite o segundo número: ");
-            scanf("%lf", &valor2);
-            multiplicacao = valor1 * valor2;
-            printf("\n");
-            printf("O resultado da multiplicação de %.2lf e %.2lf é: %2.lf", valor1, valor2, multiplicacao);
-            break;
+    //Terceiro caso: Usuário escolheu a operação de multiplicação:
+    case 3:
+        printf("Digite o primeiro número: ");
+        scanf("%f", &valor1);
+        printf("Digite o segundo número: ");
+        scanf("%f", &valor2);
+        multiplicacao = valor1 * valor2;
+        printf("\n");
+        printf("O resultado da multiplicação de %.2f e %.2f é: %.2f", valor1, valor2, multiplicacao);
+        break;
 
-        //Quarto caso: Usuário escolheu a operação de divisão:
-        case 4:
-            printf("Digite o primeiro número: ");
-            scanf("%lf", &valor1);
-            printf("Digite o segundo número: ");
-            scanf("%lf", &valor2);
+    //Quarto caso: Usuário escolheu a operação de divisão:
+    case 4:
+        printf("Digite o primeiro número: ");
+        scanf("%f", &valor1);
+        printf("Digite o segundo número: ");
+        scanf("%f", &valor2);
+        if (valor2 != 0) {
             divisao = valor1 / valor2;
             printf("\n");
-            printf("O resultado da divisao de %.2lf e %.2lf é: %2.lf", valor1, valor2, divisao);
-            break;
-
-        //Quinto caso: Usuário escolheu a operação de potência:
-        case 5:
-            printf("Digite o valor da base: ");
-            scanf("%lf", &valor1);
-            printf("Digite o valor do expoente : ");
-            scanf("%lf", &valor2);
-            potencia = pow(valor1, valor2);
+            printf("O resultado da divisão de %.2f e %.2f é: %.2f", valor1, valor2, divisao);
+        }
+        else {
             printf("\n");
-            printf("O resultado da potencia de %.2lf e %.2lf é: %2.lf", valor1, valor2, potencia);
-            break;
+            printf("Inválido, escolha um valor diferente de 0.");
+        }
+        break;
 
-        //Sexto caso: Usuário escolheu a operação de raiz quadrada:
-        case 6:
-            printf("Digite o número que deseja encontrar a raiz: ");
-            scanf("%lf", &valor1);
-            raiz = sqrt(valor1);
-            printf("A raiz quadrada de %.2lf é: %.4lf", valor1, raiz);
-            break;
+    //Quinto caso: Usuário escolheu a operação de potência:
+    case 5:
+        printf("Digite o valor da base: ");
+        scanf("%f", &valor1);
+        printf("Digite o valor do expoente : ");
+        scanf("%f", &valor2);
+        potencia = pow(valor1, valor2);
+        printf("\n");
+        printf("O resultado da potencia de %.2f e %.2f é: %.2f", valor1, valor2, potencia);
+        break;
 
-        //Saída padrão para caso o usuário não insira nenhum valor de entrada que corresponda aos casos acima:
-        default:
-            printf("Por favor, tente novamente inserindo uma opção válida.");
+    //Sexto caso: Usuário escolheu a operação de raiz quadrada:
+    case 6:
+        printf("Digite o número que deseja encontrar a raiz: ");
+        scanf("%f", &valor1);
+        raiz = sqrt(valor1);
+        printf("A raiz quadrada de %.2f é: %.2f", valor1, raiz);
+        break;
+
+    //Saída padrão para caso o usuário não insira nenhum valor de entrada que corresponda aos casos acima:
+    default:
+        printf("Por favor, tente novamente inserindo uma opção válida.");
     }
     return 0;
 }
